0e077f9d03cdffb229c6d796c5c35385fc305635
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-07-07 11:36:44 -0600

    Refactor single-quote to double-quote

--- test/sixarm_ruby_mysql_adapter_extensions_test.rb
+++ test/sixarm_ruby_mysql_adapter_extensions_test.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-require 'minitest/autorun'
-require 'simplecov'
+require "minitest/autorun"
+require "simplecov"
 SimpleCov.start
-require 'sixarm_ruby_mysql_adapter_extensions'
+require "sixarm_ruby_mysql_adapter_extensions"
 
 class MysqlAdapterExtensionsTest < Test::Unit::TestCase
 
